8c8d6964a9265728a5ee13329fe79ccb1a2b1450
Soumya Ranjan Mohanty <soumyaranjanmohanty@Soumyas-Laptop.local>
2025-12-22 13:00:29 +0530

    hompage issue resolved

--- server/config/db.js
+++ server/config/db.js
@@ -1,22 +1,34 @@
-// server/config/db.js
-const { Pool } = require('pg');
-require('dotenv').config();
+// // server/config/db.js
+// const { Pool } = require('pg');
+// require('dotenv').config();
 
-// Configuration using environment variables
+// // Configuration using environment variables
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
+// pool.connect((err, client, release) => {
+//     if (err) {
+//         return console.error('Error acquiring client', err.stack);
+//     }
+//     console.log('Successfully connected to PostgreSQL database!');
+//     release(); // Release the client back to the pool
+// });
+
+// module.exports = pool;
+
+import pkg from "pg";
+const { Pool } = pkg;
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error('Error acquiring client', err.stack);
-    }
-    console.log('Successfully connected to PostgreSQL database!');
-    release(); // Release the client back to the pool
-});
-
-module.exports = pool;
+export default pool;
